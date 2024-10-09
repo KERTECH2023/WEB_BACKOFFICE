b7ac08682fefd326bded99734330fb1a06c15d51
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-10-09 16:38:24 +0100

    Update Table.jsx

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -39,7 +39,7 @@ const [isImageOpen, setIsImageOpen] = useState(false);
   console.log("useriddd", id);
   
   const getSingleUser = async (id)  => {
-    const response = await axios.get(process.env.NEXT_PUBLIC_BASE_URL + `/Chauff/searchchauf/${id}`);
+    const response = await axios.get(process.env.REACT_APP_BASE_URL + `/Chauff/searchchauf/${id}`);
     if(response.status===200){
    setUser({ ...response.data })
    console.log("data" , response.data)
