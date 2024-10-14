361103242acb468825aedac7e71748046e78dcf6
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-10-14 16:44:59 +0100

    Update ListTraif.jsx

--- frontend/src/pages/ListTarif/ListTraif.jsx
+++ frontend/src/pages/ListTarif/ListTraif.jsx
@@ -1,51 +1,37 @@
 import React, { useState } from 'react';
-import "./listtraif.scss";
-import Sidebar from "../../components/sidebar/Sidebar";
-import Navbar from "../../components/navbar/Navbar";
-import DataTarif from "../../components/DataTarif/DataTarif";
+import './listtraif.scss';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import DataTarif from '../../components/DataTarif/DataTarif';
 
 const ListTraif = () => {
-    const [showForm, setShowForm] = useState(false);
+  const [tarifName, setTarifName] = useState('');
 
-    const handleAddClick = () => {
-        setShowForm(!showForm);
-    };
+  const handleAddTarif = () => {
+    // Logique pour ajouter le tarif
+    console.log('Ajouter Tarif:', tarifName);
+    // Ici, vous pouvez appeler une API ou mettre à jour l'état global
+    setTarifName('');
+  };
 
-    return (
-        <div className="list">
-            <Sidebar/>
-            <div className="listContainer">
-                <Navbar/>
-                <button onClick={handleAddClick}>
-                    {showForm ? "Fermer le formulaire" : "Ajouter un tarif"}
-                </button>
-                {showForm && <TarifForm />}
-                <DataTarif/>
-            </div>
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <div className="formContainer">
+          <input
+            type="text"
+            value={tarifName}
+            onChange={(e) => setTarifName(e.target.value)}
+            placeholder="Nom du tarif"
+          />
+          <button onClick={handleAddTarif}>Ajouter Tarif</button>
         </div>
-    );
-}
-
-const TarifForm = () => {
-    const [tarif, setTarif] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Logique pour soumettre le tarif
-        console.log("Tarif ajouté:", tarif);
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <input 
-                type="text" 
-                value={tarif} 
-                onChange={(e) => setTarif(e.target.value)} 
-                placeholder="Entrez le tarif" 
-            />
-            <button type="submit">Ajouter</button>
-        </form>
-    );
+        <DataTarif />
+      </div>
+    </div>
+  );
 };
 
 export default ListTraif;
