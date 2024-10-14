103278758bb1da57936a0657e51297b26e3bebc2
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-10-14 17:01:29 +0100

    Update ListTraif.jsx

--- frontend/src/pages/ListTarif/ListTraif.jsx
+++ frontend/src/pages/ListTarif/ListTraif.jsx
@@ -1,37 +1,19 @@
-import React, { useState } from 'react';
-import './listtraif.scss';
-import Sidebar from '../../components/sidebar/Sidebar';
-import Navbar from '../../components/navbar/Navbar';
-import DataTarif from '../../components/DataTarif/DataTarif';
+import React from 'react'
+import "./listtraif.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
 
+import DataTarif from "../../components/DataTarif/DataTarif"
 const ListTraif = () => {
-  const [tarifName, setTarifName] = useState('');
-
-  const handleAddTarif = () => {
-    // Logique pour ajouter le tarif
-    console.log('Ajouter Tarif:', tarifName);
-    // Ici, vous pouvez appeler une API ou mettre à jour l'état global
-    setTarifName('');
-  };
-
-  return (
-    <div className="list">
-      <Sidebar />
-      <div className="listContainer">
-        <Navbar />
-        <div className="formContainer">
-          <input
-            type="text"
-            value={tarifName}
-            onChange={(e) => setTarifName(e.target.value)}
-            placeholder="Nom du tarif"
-          />
-          <button onClick={handleAddTarif}>Ajouter Tarif</button>
+    return (
+        <div className="list">
+          <Sidebar/>
+          <div className="listContainer">
+            <Navbar/>
+            <DataTarif/>
+          </div>
         </div>
-        <DataTarif />
-      </div>
-    </div>
-  );
-};
+      )
+}
 
-export default ListTraif;
+export default ListTraif
