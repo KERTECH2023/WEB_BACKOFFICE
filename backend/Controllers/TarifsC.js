9ce9570f479d7d618281ad1cbaf7651d70e208a8
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-10-15 09:43:10 +0100

    Update TarifsC.js

--- backend/Controllers/TarifsC.js
+++ backend/Controllers/TarifsC.js
@@ -22,7 +22,7 @@ exports.addTarifAndUpdateChauffeurs = async (req, res, next) => {
             });
         }
 
-        const newTarif = new Tarifs({ tarif, tarifmaj });
+        const newTarif = new Tarifs({ tarif, tarifMaj });
         const savedTarif = await newTarif.save();
 
         const tariffId = savedTarif._id;
