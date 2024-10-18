e138e89849ba29eb95d99fbbf1a3209f34ae037c
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-10-18 11:46:45 +0100

    Update factureRoutes.js

--- backend/routes/factureRoutes.js
+++ backend/routes/factureRoutes.js
@@ -15,6 +15,6 @@ router.get('/chauffeur/:driverId/mois', factureController.getFacturesForDriverTh
 router.get('/chauffeur/:driverId/pdf', factureController.getFacturePDF);
 
 // Nouvelle route pour mettre à jour le statut de la facture à "PAYE"
-router.patch('/:id/payer', factureController.updateFactureToPaid);
+router.patch('/:driverId/payer', factureController.updateFactureToPaid);
 
 module.exports = router;
