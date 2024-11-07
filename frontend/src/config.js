4cabadb5ca0d7a43c8763e02afb1e5dc747fda20
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-11-07 18:21:47 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,10 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOLPeAJY2yo8-9QuT275KFsCGHEWEqVjs",
-  projectId: "transport-app-36443",
-  storageBucket: "transport-app-36443.appspot.com",
-};
+    apiKey: "AIzaSyDgBbTBbjIo8hVD128y5TG9FswyZVO8XGE",
+    authDomain: "prd-transport.firebaseapp.com",
+    databaseURL: "https://prd-transport-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "prd-transport",
+    storageBucket: "prd-transport.appspot.com",
+    messagingSenderId: "824880668007",
+    appId: "1:824880668007:web:51fd327f9a2da5e4c0a3c2",
+    measurementId: "G-H6XHHDMSSH"
+  };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
