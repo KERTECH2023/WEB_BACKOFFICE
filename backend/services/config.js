a1ae1581d79f68175c33aacb4b8f827c9acc4f2d
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2024-11-21 16:03:06 +0100

    Update config.js

--- backend/services/config.js
+++ backend/services/config.js
@@ -6,55 +6,55 @@ require("dotenv").config();
 
 const BUCKET = "transport-app-36443.appspot.com";
 
-
-
+async function initializeServices() {
   const client = new MongoClient(config.database);
 
- 
-    await client.connect();
-    const db = client.db();
-    const collection = db.collection("firebasekey");
-    const firebaseKey = await collection.findOne();
+  await client.connect();
+  const db = client.db();
+  const collection = db.collection("firebasekey");
+  const firebaseKey = await collection.findOne();
 
-    if (!firebaseKey) {
-      throw new Error("Firebase key not found in database");
-    }
+  if (!firebaseKey) {
+    throw new Error("Firebase key not found in database");
+  }
 
-    const firebaseConfig = {
-       type: firebaseKey.type,
-            projectId: firebaseKey.project_id,
-            privateKeyId: firebaseKey.private_key_id,
-            privateKey: firebaseKey.private_key.replace(/\\n/g, '\n'),
-            clientEmail: firebaseKey.client_email,
-            clientId: firebaseKey.client_id,
-            authUri: firebaseKey.auth_uri,
-            tokenUri: firebaseKey.token_uri,
-            authProviderCertUrl: firebaseKey.auth_provider_x509_cert_url,
-            clientCertUrl: firebaseKey.client_x509_cert_url,
-    };
+  const firebaseConfig = {
+    type: firebaseKey.type,
+    projectId: firebaseKey.project_id,
+    privateKeyId: firebaseKey.private_key_id,
+    privateKey: firebaseKey.private_key.replace(/\\n/g, "\n"),
+    clientEmail: firebaseKey.client_email,
+    clientId: firebaseKey.client_id,
+    authUri: firebaseKey.auth_uri,
+    tokenUri: firebaseKey.token_uri,
+    authProviderCertUrl: firebaseKey.auth_provider_x509_cert_url,
+    clientCertUrl: firebaseKey.client_x509_cert_url,
+  };
 
-    const firestoreApp = admin.initializeApp(
-      {
-        credential: admin.credential.cert(firebaseConfig),
-        databaseURL: "https://prd-transport-default-rtdb.europe-west1.firebasedatabase.app",
-      },
-      "firestoreApp"
-    );
-
-    admin.initializeApp({
+  const firestoreApp = admin.initializeApp(
+    {
       credential: admin.credential.cert(firebaseConfig),
-      storageBucket: BUCKET,
-    });
+      databaseURL: "https://prd-transport-default-rtdb.europe-west1.firebasedatabase.app",
+    },
+    "firestoreApp"
+  );
 
-    const bucket = admin.storage().bucket();
-    const firestore = admin.firestore();
+  admin.initializeApp({
+    credential: admin.credential.cert(firebaseConfig),
+    storageBucket: BUCKET,
+  });
 
-    
- 
+  const bucket = admin.storage().bucket();
+  const firestore = admin.firestore();
 
-    module.exports = {
-      admin,
-      firestoreApp,
-      db,
-      bucket,
-    };
+  return { admin, firestoreApp, db, bucket };
+}
+
+initializeServices()
+  .then((services) => {
+    module.exports = services;
+  })
+  .catch((err) => {
+    console.error("Failed to initialize services:", err);
+    process.exit(1);
+  });
