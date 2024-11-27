0aa03f94d1a115099b3dc3687ce592a7980cc21b
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2024-11-27 11:35:22 +0100

    Update AllRideRequest.js

--- backend/Models/AllRideRequest.js
+++ backend/Models/AllRideRequest.js
@@ -1,72 +1,105 @@
-const mongoose = require('mongoose');
-const Facture = require('./Facture');
-const Chauffeur = require("./Chauffeur");
+const mongoose = require("mongoose");
 
 const rideRequestSchema = new mongoose.Schema({
-  
-  HealthStatus: {
+  firebaseRiderRequestsID: {
     type: String,
-    default: 'None'
+    required: true,
+  },
+  driverId: {
+    type: String,
+    required: true,
+  },
+  driverName: {
+    type: String,
+    required: true,
+  },
+  driverPhone: {
+    type: String,
+    required: true,
+  },
+  driverPhoto: {
+    type: String,
+    required: true,
+  },
+  driverLocationData: {
+    latitude: {
+      type: String,
+      required: true,
+    },
+    longitude: {
+      type: String,
+      required: true,
+    },
+  },
+  carDetails: {
+    carModel: {
+      type: String,
+      required: true,
+    },
+    carNumber: {
+      type: String,
+      required: true,
+    },
+  },
+  fareAmount: {
+    type: Number,
+    required: true,
+  },
+  healthStatus: {
+    type: String,
+    required: true,
+    default: "none",
+  },
+  source: {
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true,
+    },
+  },
+  sourceAddress: {
+    type: String,
+    required: true,
   },
   destination: {
     latitude: {
       type: Number,
-      
+      required: true,
     },
     longitude: {
       type: Number,
-      
-    }
-    
-  },
-  destinationAddress: {
-    type: String,
-    
-  },
-  driverLocationData: {
-    latitude: {
-      type: Number,
-      
+      required: true,
     },
-    longitude: {
-      type: Number,
-      
-    }
-    
-  },
-  fareAmount:{
-    type:Number,
-  },
-   
-    driverPhone: {
+    destinationAddress: {
       type: String,
-      
+      required: true,
     },
-  source: {
-    latitude: {
-      type: Number,
-    },
-    longitude: {
-      type: Number,
-    }
   },
   status: {
     type: String,
+    required: true,
   },
   time: {
-    type: Date,
-    
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
   },
   userName: {
     type: String,
+    required: true,
   },
   userPhone: {
     type: String,
-  }
-});
+    required: true,
+  },
+}, { timestamps: true });
 
-
-
-const RideRequest = mongoose.model('RideRequest', rideRequestSchema);
+const RideRequest = mongoose.model("RideRequest", rideRequestSchema);
 
 module.exports = RideRequest;
