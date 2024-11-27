2e193d07f20b8511ea5aeaed93dd680bade65f8e
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2024-11-27 11:36:41 +0100

    Update RideRoute.js

--- backend/routes/RideRoute.js
+++ backend/routes/RideRoute.js
@@ -4,6 +4,7 @@ const  router = express.Router()
 
 const RideContro  = require('../Controllers/RideController')
 router.post('/postRide', RideContro.saveRide);
-//router.get('/fact', RideContro.getfact)
+router.post('/firebaseToMongoDB', RideContro.saveRideFirebaseToMongoDB);
+
 
 module.exports = router
