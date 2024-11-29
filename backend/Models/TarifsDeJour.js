40d8b7c1f004fd0817373a689cffaf5ec165f798
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2024-11-29 14:29:31 +0100

    Add files via upload

---
+++ backend/Models/TarifsDeJour.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const tarifjourSchema = new mongoose.Schema({
+  
+  baseFare: {
+    type: Number,
+    required: true
+  },
+  farePerKm: {
+    type: Number,
+    
+  },
+  farePerMinute: {
+    type: Number,
+    
+  },
+  
+  // Add other properties specific to your tarifjourf model if needed
+});
+tarifjourSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+      delete ret._id;
+      
+  }
+});
+
+const Tarifsjour = mongoose.model('Tarifjour', tarifjourSchema);
+
+module.exports = Tarifsjour;
---
+++ backend/Models/TarifsDeNuit.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const tarifnuitSchema = new mongoose.Schema({
+  
+  baseFare: {
+    type: Number,
+    required: true
+  },
+  farePerKm: {
+    type: Number,
+    
+  },
+  farePerMinute: {
+    type: Number,
+    
+  },
+  
+  // Add other properties specific to your tarifnuitf model if needed
+});
+tarifnuitSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret) {
+      delete ret._id;
+      
+  }
+});
+
+const Tarifsnuit = mongoose.model('Tarifnuit', tarifnuitSchema);
+
+module.exports = Tarifsnuit;
