0578da2625a0f2de20cd93902e5a9cb234093d67
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-12-23 16:16:46 +0100

    Update ClientContro.js

--- backend/Controllers/ClientContro.js
+++ backend/Controllers/ClientContro.js
@@ -471,8 +471,27 @@ const Clientdesa = async(req,res,data) =>{
 }
 /**----------------------Supprimer un agent------------------- */
 
+const ClientSchema = new mongoose.Schema({
+  firebaseUID: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  healthStatus: {
+    type: String,
+    default: "none"
+  },
+  email: String,
+  name: String,
+  phone: String,
+  token: String,
+  factures: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Facture'
+  }]
+});
 
-
+const Client = mongoose.model('Client', ClientSchema);
 
 async function syncClientsFirebaseToMongoDB(req, res) {
   try {
