18b3a01b776b733259c23ed55e3e4358f6bc6800
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-12-23 12:27:53 +0100

    Update DataClient.jsx

--- frontend/src/components/dataClient/DataClient.jsx
+++ frontend/src/components/dataClient/DataClient.jsx
@@ -7,7 +7,6 @@ import { React, useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import "react-toastify/dist/ReactToastify.css";
-import { initializeApp } from "firebase/app";
 import { database } from "../../config"; 
 import { ref, onValue } from "firebase/database";
 
