fa407dad81b235e501a36202f1131970bc61b6c2
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2024-12-26 14:49:06 +0100

    Update RideRoute.js

--- backend/routes/RideRoute.js
+++ backend/routes/RideRoute.js
@@ -6,5 +6,6 @@ const RideContro  = require('../Controllers/RideController')
 router.post('/postRide', RideContro.saveRide);
 router.post('/firebaseToMongoDB', RideContro.saveRideFirebaseToMongoDB);
 router.get('/ride-requests/:userId', RideContro.getRideRequestsByUserId);
+router.get('/allRideRequests', RideContro.getAllRideRequests);
 
 module.exports = router
