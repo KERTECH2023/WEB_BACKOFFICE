efe0084de765924a706d31e0c2ef51aa56574a1a
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2025-01-13 17:23:33 +0100

    Update tarifsRoutes.js

--- backend/routes/tarifsRoutes.js
+++ backend/routes/tarifsRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const TarifsDeTempsFortController = require('../Controllers/TarifsDeTempsFortController');
 
-router.get('/show', TarifsDeTempsFortController.showPeakTarifs);
-router.post('/tarif', TarifsDeTempsFortController.addPeakTarifAndUpdateChauffeurs);
-router.put('/update/:id', TarifsDeTempsFortController.updatePeakTarif);
+router.get('/show', tarifsTempsFortController.showtarifs);
+
+// Route pour ajouter ou mettre à jour un tarif et mettre à jour les chauffeurs
+router.post('/tarif', tarifsTempsFortController.addTarifAndUpdateChauffeurs);
+
+// Route pour mettre à jour un tarif existant
+router.put('/update', tarifsTempsFortController.updateTarifAndMajoration);
 module.exports = router;
