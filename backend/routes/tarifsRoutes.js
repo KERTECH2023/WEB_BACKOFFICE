0eec857d015a41e8a16802b273debbb21d16606c
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2025-01-13 17:13:58 +0100

    Update tarifsRoutes.js

--- backend/routes/tarifsRoutes.js
+++ backend/routes/tarifsRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const TarifsDeTempsFortController = require('../Controllers/TarifsDeTempsFortController');
 
 router.get('/show', TarifsDeTempsFortController.showPeakTarifs);
-router.post('/add', TarifsDeTempsFortController.addPeakTarifAndUpdateChauffeurs);
+router.post('/tarif', TarifsDeTempsFortController.addPeakTarifAndUpdateChauffeurs);
 router.put('/update/:id', TarifsDeTempsFortController.updatePeakTarif);
 module.exports = router;
