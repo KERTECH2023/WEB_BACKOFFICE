2d8178cc5dee4cb11aa7a47086e498631714c2e1
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2025-01-14 15:39:27 +0100

    Update SoldeRoute.js

--- backend/routes/SoldeRoute.js
+++ backend/routes/SoldeRoute.js
@@ -3,6 +3,7 @@ const syncSoldeData = require("../Controllers/SoldeController");
 const router = express.Router();
 
 router.get("/solde/:driverId", syncSoldeData.getSoldeById);
+router.get("/soldetotal", syncSoldeData.getTotalSolde);
 
 module.exports = router;
 
