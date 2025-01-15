4b9066feb632b2c5a6c6e97c243be3574f21eca2
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2025-01-15 08:59:43 +0100

    Update SoldeRoute.js

--- backend/routes/SoldeRoute.js
+++ backend/routes/SoldeRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/solde/:driverId", syncSoldeData.getSoldeById);
 router.get("/soldetotal", syncSoldeData.getTotalSolde);
-router.put("/update/:driverId", syncSoldeData.updateSolde);
+router.put("/solde/:driverId", syncSoldeData.updateSolde);
 
 module.exports = router;
 
