361df99a1076fd0bde27e3dbbddaa54acbf8dadf
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2025-02-03 14:39:58 +0100

    Add files via upload

---
+++ backend/routes/rideRequests.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const RideRequests = require("../controllers/rideRequestsController");
+
+// Route pour récupérer toutes les demandes
+router.get("/ride-requests", RideRequests.getAllRideRequests);
+
+// Route pour mettre à jour une demande spécifique
+router.put("/ride-requests/:rideRequestId", RideRequests.updateRideRequest);
+
+// Route pour supprimer une demande spécifique
+router.delete("/ride-requests/:rideRequestId", RideRequests.deleteRideRequest);
+
+module.exports = router;
