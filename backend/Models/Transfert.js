0a056f3c75d38fd8734876517a6136276ce0652a
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-02-06 16:14:12 +0100

    Update Transfert.js

--- backend/Models/Transfert.js
+++ backend/Models/Transfert.js
@@ -7,14 +7,17 @@ const TransferSchema = new mongoose.Schema({
   lastName: { type: String},
   email: { type: String},
   phone: { type: String },
+  bagageCabine: { type: String },
+  bagageSoute: { type: String },
+  bagageHorsFormat: { type: String },
   datevol: { type: String },
   heurvol: { type: String },
   numvol: { type: String },
   airport: { type: String },
   destination: { type: String },
   passengers: { type: String},
+  accepter: { type: String, default: 'non_accepter' },
   price: { type: String },
-  accepter: { type: String },
   createdAt: { type: Date, default: Date.now }
 });
 
