2c6e4c069e6d565399fa6f1ba4f3722f27113cdd
douahemaymen <119676859+douahemaymen@users.noreply.github.com>
2025-02-20 15:36:34 +0100

    Update ReservationTaxiContro.js

--- backend/Controllers/ReservationTaxiContro.js
+++ backend/Controllers/ReservationTaxiContro.js
@@ -2,21 +2,17 @@ const firestoreModule = require("../services/config");
 const realtimeDB = firestoreModule.firestoreApp.database();
 
 const stripe = require("stripe")("sk_test_51QuAbpQFlhR6CoMtocz2YCH80ULE8hY5412hALJsZxXDDfJ6QovCfivUPH1W9EagiflIa7EHzgDzCn0QVJaJ7K8Z00I37p2IO8"); // Remplace "sk_test_xxx" par ta vraie clé
-exports.testCard = async (req, res) => {
+const createPaymentIntent = async (req, res) => {
   try {
-    const { cardNumber, expMonth, expYear, cvc } = req.body;
+    const { amount, currency } = req.body;
 
-    // Création d'un token pour tester la validité de la carte
-    const token = await stripe.tokens.create({
-      card: {
-        number: cardNumber,
-        exp_month: expMonth,
-        exp_year: expYear,
-        cvc: cvc,
-      },
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: currency,
+      payment_method_types: ["card"],
     });
 
-    res.json({ success: true, message: "Carte valide", token: token.id });
+    res.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
