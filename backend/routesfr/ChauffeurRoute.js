0ba4d1b127192f07de484d1c5853b6de517cc0a6
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-03 13:07:17 +0100

    Update ChauffeurRoute.js

--- backend/routesfr/ChauffeurRoute.js
+++ backend/routesfr/ChauffeurRoute.js
@@ -1,95 +1,202 @@
-const express = require("express");
-const router = express.Router();
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+require('dotenv').config()
 
-const ChauffContro = require("../Controllersfr/ChauffContro");
-const tarifContro = require("../Controllersfr/TarifsC");
+const { db2france } = require("./configbasefrance");
 
-//const UploadImage = require ("../services/firebase");
-const UploadImage = require("../services/firebase");
+const { synchronizeData } = require("./Controllers/ChauffContro");
 
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const Multer = multer({
-  storage: multer.memoryStorage(),
-  limits: 1024 * 1024,
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const config = require("./config.json");
+const AuthRoute = require("./routes/adminRoutes");
+const Agentrout = require("./routes/AgentRoute");
+const Agentchauff = require("./routes/ChauffeurRoute");
+const ClRoute = require("./routes/ClientRoute");
+const Rec = require("./routes/ReclamationRout");
+const Voi = require("./routes/VoitureRoutes");
+
+const tar = require("./routes/TarifRoute");
+const tarj = require("./routes/TarifRouteJour.js");
+const tart = require("./routes/tarifsRoutes.js");
+const tarn = require("./routes/TarifRouteNuit.js");
+const transfert = require("./routes/TransfertRoutes.js");
+const Solde = require("./routes/SoldeRoute.js");
+const rideRequests = require("./routes/rideRequests.js");
+const tariftransfet = require("./routes/TariftransfertRoute.js");
+const reservationTaxi = require("./routes/ReservationTaxiRoutes.js");
+const con = require("./routes/ContactRoute");
+const rides = require("./routes/RideRoute");
+const factureRoute = require("./routes/factureRoutes.js")
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const { runAggregation } = require("./Controllers/RideController"); // Export runAggregation function
+const {
+  generateFactures,
+  generateDriverStatistics,
+} = require("./Controllers/FactureController");
+
+
+
+
+
+
+
+
+
+
+const { synchronizeDatafr } = require("./Controllersfr/ChauffContro");
+
+
+const AuthRoutefr = require("./routesfr/adminRoutes");
+const Agentroutfr = require("./routesfr/AgentRoute");
+const Agentchaufffr = require("./routesfr/ChauffeurRoute");
+const ClRoutefr = require("./routesfr/ClientRoute");
+const Recfr = require("./routesfr/ReclamationRout");
+const Voifr = require("./routesfr/VoitureRoutes");
+const tarfr = require("./routesfr/TarifRoute");
+const tarjfr = require("./routesfr/TarifRouteJour.js");
+const tartfr = require("./routesfr/tarifsRoutes.js");
+const tarnfr = require("./routesfr/TarifRouteNuit.js");
+const transfertfr = require("./routesfr/TransfertRoutes.js");
+const Soldefr = require("./routesfr/SoldeRoute.js");
+const rideRequestsfr = require("./routesfr/rideRequests.js");
+const tariftransfetfr = require("./routesfr/TariftransfertRoute.js");
+const reservationTaxifr = require("./routesfr/ReservationTaxiRoutes.js");
+const confr = require("./routesfr/ContactRoute");
+const ridesfr = require("./routesfr/RideRoute");
+
+var indexRouterfr = require("./routesfr/index");
+var usersRouterfr = require("./routesfr/users");
+const { runAggregationfr } = require("./Controllersfr/RideController"); // Export runAggregation function
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+db.on("error", (err) => {
+  console.log(err);
+});
+
+db.once("open", () => {
+  console.log("DB Connection Estabblished !");
+});
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(logger("dev"));
+const corsOptions = {
+  // origin:'https://front-admin-vert.vercel.app',
+
+  origin: "*",
+
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api", AuthRoute);
+app.use("/agent", Agentrout);
+app.use("/Chauff", Agentchauff);
+app.use("/Voifr", VoiFR);
+app.use("/Client", ClRoute);
+app.use("/Rec", Rec);
+app.use("/Voi", Voi);
+app.use("/Tar", tar);
+app.use("/transfer", transfert);
+app.use("/tariftransfet", tariftransfet);
+app.use("/reservationTaxi", reservationTaxi);
+
+app.use("/Tarj", tarj);
+app.use("/Tart", tart);
+app.use("/Tarn", tarn);
+app.use("/Solde", Solde);
+app.use("/rideRequests", rideRequests);
+app.use("/Con", con);
+app.use("/facture",factureRoute);
+
+app.use("/Ride", rides);
+
+app.get("/testAggregation", async (req, res) => {
+  try {
+    // await runAggregation();
+    await generateFactures();
+    const stats = await generateDriverStatistics();
+    res
+      .status(200)
+      .json({
+        message: "Aggregation executed successfully.",
+        statistics: stats,
+      });
+  } catch (error) {
+    console.error("Error during manual aggregation:", error);
+    res.status(500).json({ message: "Error during manual aggregation." });
+  }
 });
 
 
 
-
-router.get("/affiche", ChauffContro.recupereruse);
-
-router.get("/getchdes", ChauffContro.chauffdes);
-
-router.delete("/destroychauff/:id", ChauffContro.destroy);
-
-// router.post('/AjoutChauf',Multer.single("photoAvatar"),UploadImage,ChauffContro.register)
-
-router.post(
-  "/AjoutChauf",
-  Multer.fields([
-    { name: "photoAvatar", maxCount: 1 },
-    { name: "photoPermisRec", maxCount: 1 },
-    { name: "photoPermisVer", maxCount: 1 },
-    { name: "photoVtc", maxCount: 1 },
-    { name: "photoCin", maxCount: 1 },
-  ]),
-  UploadImage,
-  ChauffContro.register
-);
+// france
 
 
-router.post(
-  "/sendnotificationchauffeur",
-  ChauffContro.sendmessagingnotification
-);
 
-router.post(
-  "/sendmiseajour",
-  ChauffContro.sendNotificationMiseajour
-);
+app.use("/fr", indexRouterfr);
+app.use("/usersfr", usersRouterfr);
+app.use("/apifr", AuthRoutefr);
+app.use("/agentfr", Agentroutfr);
+app.use("/Chaufffr", Agentchaufffr);
+app.use("/Clientfr", ClRoutefr);
+app.use("/Recfr", Recfr);
+app.use("/Voifr", Voifr);
+app.use("/Tarfr", tarfr);
+app.use("/transferfr", transfertfr);
+app.use("/tariftransfetfr", tariftransfetfr);
+app.use("/reservationTaxifr", reservationTaxifr);
 
-router.post(
-  "/sendnotificationclient",
-  ChauffContro.sendmessagingnotificationclient
-);
-
-router.put(
-  "/updatechauf/:id",
-  Multer.fields([
-    { name: "photoAvatar", maxCount: 1 },
-    { name: "photoPermisRec", maxCount: 1 },
-    { name: "photoPermisVer", maxCount: 1 },
-    { name: "photoVtc", maxCount: 1 },
-    { name: "photoCin", maxCount: 1 },
-  ]),
-  UploadImage,
-  ChauffContro.update
-);
-
-router.post("/loginch", ChauffContro.login);
-router.get("/searchchauf/:id", ChauffContro.searchuse);
-router.get("/newchauf", ChauffContro.recuperernewchauf);
-//router.get('/getAg', AuthController.recupereruse);
-// router.put('/updatechauf/:id',Multer.single("photoAvatar"),UploadImage, ChauffContro.update);
-router.put("/updatestatus/:id", ChauffContro.updatestatus);
-router.put('/reactivate-chauffeur/:id', ChauffContro.reactivateChauffeur);
-
-router.put("/updatestatuss/:id", ChauffContro.updatestatuss);
-
-router.post('/:id/reject', ChauffContro.rejectChauffeur);
-//router.options('/facture-amounts',factureUpdate);
-//router.get('/factures/:chauffeurId', ChauffContro.getFacturesByChauffeurId);
+app.use("/Tarjfr", tarjfr);
+app.use("/Tartfr", tartfr);
+app.use("/Tarnfr", tarnfr);
+app.use("/Soldefr", Soldefr);
+app.use("/rideRequestsfr", rideRequestsfr);
+app.use("/Confr", confr);
 
 
-router.post(
-  "/sendFacture",
-  upload.single("file"),
-  ChauffContro.sendFactureEmail
-);
-router.get("/rideCounts", ChauffContro.getRideCounts);
-router.put("/updatefacture/:id", ChauffContro.updateFact);
-router.put("/updatef/:id", ChauffContro.updateF);
+app.use("/Ridefr", ridesfr);
 
-module.exports = router;
+
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// Define an asynchronous function to start the server and initiate data synchronization
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Call the startServer function to start the server
+
+module.exports = app;
