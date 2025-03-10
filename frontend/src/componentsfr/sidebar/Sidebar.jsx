df2fe29bd44dba1e9ddfaeaa2ca9a99b8c57a0ec
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-10 14:36:20 +0100

    Update Sidebar.jsx

--- frontend/src/componentsfr/sidebar/Sidebar.jsx
+++ frontend/src/componentsfr/sidebar/Sidebar.jsx
@@ -70,7 +70,7 @@ const Sidebar = () => {
           )}
      <Link to="/gpsfr" style={{ textDecoration: "none" }}>
             <li>
-              <LocationOnIcon className="icon" />
+              <AccessibilityIcon className="icon" />
               <span>gps taxi</span>
             </li>
           </Link>
