1e41f4e7546a34e0fa596a846b0a1f2aa593a11b
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-10 14:20:52 +0100

    Update Sidebar.jsx

--- frontend/src/componentsfr/sidebar/Sidebar.jsx
+++ frontend/src/componentsfr/sidebar/Sidebar.jsx
@@ -68,7 +68,12 @@ const Sidebar = () => {
             </li>
           </Link>
           )}
-
+     <Link to="/gpsfr" style={{ textDecoration: "none" }}>
+            <li>
+              <LocationOnIcon className="icon" />
+              <span>gps taxi</span>
+            </li>
+          </Link>
           <Link to="/Chauffeurfr" style={{ textDecoration: "none" }}>
             <li>
               <AirlineSeatReclineNormalIcon className="icon" />
