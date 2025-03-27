d9b96c2a338f8c926e52d567f6f0e15a8f9b2a2c
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-03-27 10:08:51 +0100

    Update paymentHistoryRoutes.js

--- backend/routesfr/paymentHistoryRoutes.js
+++ backend/routesfr/paymentHistoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const paymentHistoryController = require("../controllers/paymentHistoryController");
+const paymentHistoryController = require("../Controllersfr/paymentHistoryController");
 
 router.post("/payments", paymentHistoryController.addPayment);
 router.get("/payments", paymentHistoryController.getPaymentsHistory);
