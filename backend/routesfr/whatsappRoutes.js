844b433c625d3b18dbc283bde4b4b9954cd93a97
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-04-10 16:57:45 +0100

    Update whatsappRoutes.js

--- backend/routesfr/whatsappRoutes.js
+++ backend/routesfr/whatsappRoutes.js
@@ -5,6 +5,8 @@ const whatsappController = require('../Controllersfr/whatsappController');
 router.post('/start', whatsappController.startWhatsApp);
 router.get('/qrcode', whatsappController.getQRCode);
 router.post('/send', whatsappController.sendMessage);
+router.post('/sendchafffr', whatsappController.sendMessageaTousLesChauffeurfr);
+router.post('/sendchafftn', whatsappController.sendMessageaTousLesChauffeurtn);
 router.get('/status', whatsappController.getStatus);
 router.post('/logout', whatsappController.logoutWhatsApp);
 
