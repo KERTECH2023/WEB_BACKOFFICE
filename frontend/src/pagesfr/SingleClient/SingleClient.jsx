a13735e07a15bb1d5af61c3e660a860a2d8b9d16
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2026-05-26 10:30:10 +0100

    Update SingleClient.jsx

--- frontend/src/pagesfr/SingleClient/SingleClient.jsx
+++ frontend/src/pagesfr/SingleClient/SingleClient.jsx
@@ -35,7 +35,7 @@ const SingleClient = () => {
 
     const fetchRides = async (cnicNo) => {
         try {
-            const response = await axios.get(`https://api.backofficegc.com/Ridefr/ride-requests/${cnicNo}`);
+            const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/Ridefr/ride-requests/${cnicNo}`);
             if (response.status === 200 && response.data.rideRequests) {
                 setRides(response.data.rideRequests);
             }
